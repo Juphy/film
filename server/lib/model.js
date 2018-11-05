98d2ebc6edc5f6f4afaea9752f1c0e4c1fb7b529
wenquanpage <825414435@qq.com>
2018-11-05 10:08:36 +0800

    新增数据库表设计

--- server/lib/model.js
+++ server/lib/model.js
@@ -1,10 +1,15 @@
 let { sequelize } = require('./mysql');
 const Sequelize = require('sequelize');
-const user = sequelize.define('users', {
+
+
+
+
+
+const managers = sequelize.define('applet_managers', {
   name: {
     type: Sequelize.STRING
   },
-  nick_name: {
+  nickName: {
     type: Sequelize.STRING
   },
   phone: {
@@ -13,7 +18,7 @@ const user = sequelize.define('users', {
   password: {
     type: Sequelize.STRING
   },
-  create_time: {
+  createTime: {
     type: Sequelize.DATE
   },
   invalid: {
@@ -21,12 +26,250 @@ const user = sequelize.define('users', {
   }
 }, {
     timestamps: false,
-    tableName: 'applet_managers',
     freezeTableName: true
   });
 
-user.sync();
+managers.sync();
+
+
+
+const activities = sequelize.define('applet_activities',{
+  title:{
+    type: Sequelize.STRING
+  },
+  playbill:{
+    type: Sequelize.STRING
+  },
+  movieId:{
+    type: Sequelize.INTEGER
+  },
+  movieName:{
+    type: Sequelize.STRING
+  },
+  startDay:{
+    type: Sequelize.DATE
+  },
+  endDay:{
+    type: Sequelize.DATE
+  },
+  description:{
+    type: Sequelize.STRING
+  },
+  prizeDescription:{
+    type: Sequelize.JSON
+  },
+  otherDescription:{
+    type: Sequelize.JSON
+  },
+  status:{
+    type: Sequelize.INTEGER
+  },
+  managerId:{
+    type: Sequelize.INTEGER
+  },
+  managerName:{
+    type: Sequelize.STRING
+  },
+  createTime:{
+    type: Sequelize.DATE
+  },
+  invalid:{
+    type: Sequelize.INTEGER
+  }
+
+},{
+  timestamps:false,
+  freezeTableName:true
+});
+
+activities.sync();
+
+
+
+const address = sequelize.define('applet_address',{
+  openid:{
+    type: Sequelize.STRING
+  },
+  province:{
+    type: Sequelize.STRING
+  },
+  city:{
+    type: Sequelize.STRING
+  },
+  address:{
+    type: Sequelize.STRING
+  },
+  invalid:{
+    type: Sequelize.INTEGER
+  }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
+
+address.sync();
+
+
+
+const msgs = sequelize.define('applet_msgs',{
+  title:{
+    type: Sequelize.STRING
+  },
+  description:{
+    type: Sequelize.STRING
+  },
+  content:{
+    type: Sequelize.STRING
+  },
+  link:{
+    type: Sequelize.STRING
+  },
+  openids:{
+    type: Sequelize.JSON
+  },
+  managerId:{
+    type: Sequelize.INTEGER
+  },
+  managerName:{
+    type: Sequelize.STRING
+  },
+  createTime:{
+    type: Sequelize.DATE
+  },
+  sendTime:{
+    type: Sequelize.DATE
+  },
+  status:{
+    type: Sequelize.INTEGER
+  },
+  readStatus:{
+    type: Sequelize.INTEGER
+  },
+  invalid:{
+    type: Sequelize.INTEGER
+  }
+},{
+    timestamps: false,
+    freezeTableName: true
+});
+
+msgs.sync();
+
+
+
+const prizes = sequelize.define('applet_prizes',{
+
+  name:{
+    type: Sequelize.STRING
+  },
+  image:{
+    type: Sequelize.STRING
+  },
+  num:{
+    type: Sequelize.INTEGER
+  },
+  managerId:{
+    type: Sequelize.INTEGER
+  },
+  managerName:{
+    type: Sequelize.STRING
+  },
+  createTime:{
+    type: Sequelize.DATE
+  },
+  invalid:{
+    type: Sequelize.INTEGER
+  }
+},{
+    timestamps: false,
+    freezeTableName: true
+});
+
+prizes.sync();
+
+
+const reports = sequelize.define('applet_reports',{
+
+  openid:{  type: Sequelize.STRING  },
+  cinemaCode:{  type: Sequelize.INTEGER },
+  cinemaName:{  type: Sequelize.STRING  },
+  city:{  type: Sequelize.INTEGER },
+  cityName:{  type: Sequelize.STRING  },
+  chain:{ type: Sequelize.INTEGER },
+  chainName:{ type: Sequelize.STRING  },
+  content:{ type: Sequelize.JSON  },
+  remark:{  type: Sequelize.STRING  },
+  showDay:{ type: Sequelize.DATE  },
+  managerId:{ type: Sequelize.INTEGER  },
+  managerName:{ type: Sequelize.STRING },
+  status:{  type: Sequelize.INTEGER },
+  invalid:{ type: Sequelize.INTEGER }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
+
+reports.sync();
+
+
+
+const users = sequelize.define('applet_users',{
+
+  openid: { type: Sequelize.STRING },
+  nickName: { type: Sequelize.STRING },
+  gender: { type: Sequelize.INTEGER },
+  country: { type: Sequelize.STRING },
+  province: { type: Sequelize.STRING },
+  city: { type: Sequelize.STRING },
+  language: { type: Sequelize.STRING },
+  phone: { type: Sequelize.INTEGER },
+  avatarUrl: { type: Sequelize.STRING },
+  invalid: { type: Sequelize.INTEGER },
+  createTime: { type: Sequelize.DATE },
+  lastModify: { type: Sequelize.DATE },
+  addressId:{type: Sequelize.INTEGER},
+},{
+    timestamps: false,
+    freezeTableName: true
+});
+
+users.sync();
+
+
+
+const winners = sequelize.define('applet_winners', {
+
+  openid: { type: Sequelize.STRING },
+  activiteId: { type: Sequelize.INTEGER },
+  prizeId: { type: Sequelize.INTEGER },
+  prizeName: { type: Sequelize.STRING },
+  prizeImage: { type: Sequelize.STRING },
+  isSure: { type: Sequelize.INTEGER },
+  status: { type: Sequelize.INTEGER },
+  needDelivery: { type: Sequelize.INTEGER },
+  deliveryCode: { type: Sequelize.STRING },
+  addressId: { type: Sequelize.INTEGER },
+  address: { type: Sequelize.STRING },
+  managerId: { type: Sequelize.INTEGER },
+  managerName: { type: Sequelize.STRING },
+  isReceived: { type: Sequelize.INTEGER},
+  createTime: { type: Sequelize.DATE },
+  invalid: { type: Sequelize.INTEGER },
+}, {
+    timestamps: false,
+    freezeTableName: true
+  });
+
+winners.sync()
+
+
 
 module.exports = {
-  'manager': user
+  'manager': manager,
+  'address':address,
+  'msgs': msgs,
+  'prizes': prizes,
+  'reports': reports,
+  'users': users,
+  'winners': winners
 };
