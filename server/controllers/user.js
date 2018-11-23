d203d2f98b100bb3ef4d0f5b407f42944dcb4748
wenquanpage <825414435@qq.com>
2018-11-23 11:48:07 +0800

    提交

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -70,7 +70,7 @@ const bind_phone = async(ctx, next) => {
   console.log('-----ctx:', ctx.state.$wxInfo.userinfo)
   user_info = await User.findOne({
     where: {
-      open_id: ctx.state.$wxInfo.userinfo.userinfo.openId,
+      open_id: ctx.state.$wxInfo.userinfo.openId,
       invalid: 0
     }
   });
